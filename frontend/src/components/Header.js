299eda2642ac89a3c8f4f0f6917ca80e1ee2a76d
Michal Korzeniewski <me@mkorzeniewski.com>
2023-10-22 20:46:23 +0200

    add bootstrap; add header component

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+
+function Header({ title }) {
+  return (
+      <Navbar bg="light" variant="light">
+        <Navbar.Brand href="#home">{title}</Navbar.Brand>
+    </Navbar>
+  );
+};
+
+export default Header;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,6 +8,5 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    {a ? <App /> : <h1>Hello!!</h1>}
   </React.StrictMode>
 );
