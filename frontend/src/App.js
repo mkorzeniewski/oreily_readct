e387c3a8a69525210122170621f09487565d4a40
Michal Korzeniewski <me@mkorzeniewski.com>
2023-10-23 20:00:03 +0200

    controlled search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Search from "./components/Search";
+
+
 
 function App() {
+  const [word, setWord] = useState('');
+
+  function handleSearchSubmit(e) {
+    e.preventDefault();
+    console.log(word);
+  }
+
   return (
     <div className="App">
-      <Header title = "Images Gallery"/> 
+      <Header title="Images Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,17 @@
 import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Container } from "react-bootstrap";
+const navBarStyle = {
+  backgroundColor: "#3a86ff",
+};
 
-function Header({title}) {
+function Header({ title }) {
   return (
-      <Navbar bg="light" variant="light">
+    <Navbar style={navBarStyle} variant="light">
+      <Container>
         <Navbar.Brand href="#home">{title}</Navbar.Brand>
+      </Container>
     </Navbar>
   );
-};
+}
 
 export default Header;
