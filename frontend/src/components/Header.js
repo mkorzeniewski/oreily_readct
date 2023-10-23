3430999769b9f234f1074028e72568475f448ee8
Michal Korzeniewski <me@mkorzeniewski.com>
2023-10-23 18:05:51 +0200

    initial header component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 
-function Header({ title }) {
+function Header({title}) {
   return (
       <Navbar bg="light" variant="light">
         <Navbar.Brand href="#home">{title}</Navbar.Brand>
