d94aa0dcb67b59abfa0f2d45d9db9e79aec7da5b
Michal Korzeniewski <me@mkorzeniewski.com>
2023-10-23 20:44:31 +0200

    Use unsplash to retrieve image

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,35 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import Search from "./components/Search";
 
-
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
 
 function App() {
-  const [word, setWord] = useState('');
+  const [word, setWord] = useState("");
+
+  const [photo, setPhoto] = useState("");
 
   function handleSearchSubmit(e) {
     e.preventDefault();
     console.log(word);
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setPhoto(data.urls.regular);
+        setWord("");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   return (
     <div className="App">
       <Header title="Images Gallery" />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+      <div><img src={photo} alt="Italian Trulli"></img></div>
     </div>
   );
 }
