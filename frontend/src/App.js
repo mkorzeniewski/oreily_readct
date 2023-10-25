8fb8a0680b6c2a58ce677d5ddd7d9f59f3151c03
Michal Korzeniewski <me@mkorzeniewski.com>
2023-10-25 19:36:57 +0200

    save images in the state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,19 +7,20 @@ const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
 
 function App() {
   const [word, setWord] = useState('');
-
+  const [images, setImages] = useState([]);
   const [photo, setPhoto] = useState('');
 
   function handleSearchSubmit(e) {
     e.preventDefault();
     console.log(word);
+    console.log(images);
     fetch(
       `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`,
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         setPhoto(data.urls.regular);
+        setImages([data, ...images]);
         setWord('');
       })
       .catch((err) => {
