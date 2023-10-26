66aeb03e477767ca1840f6a2f5671e9c5817c3f6
Michal Korzeniewski <me@mkorzeniewski.com>
2023-10-26 21:22:47 +0200

    sample flask app

--- api/main.py
+++ api/main.py
@@ -1,1 +1,10 @@
-print("Hello")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "<h1>Hello, World!</h1><p>I am dev</p>"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5050)
